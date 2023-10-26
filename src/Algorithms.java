97944a8f09a30bf362d31e0809f4f4fc572bb0a0
Juan Pablo Loaiza Nieto <juanp.loaizan@uqvirtual.edu.co>
2023-10-26 14:17:01 -0500

    build: algorithms menu

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,6 +1,6 @@
 public class Algorithms {
 
-    //1-NaivStandard
+    // 1 NaivStandard
     public static void NaivStandard(int[][] A, int[][] B, int[][] Result, int N, int P, int M) {
         int aux;
         for (int i = 0; i < N; i++) {
@@ -16,11 +16,11 @@ public class Algorithms {
         }
     }
 
-    //2-NaivOnArray
+    // 2 NaivOnArray
     public static void NaivOnArray(int[][]A,int[][] B,int[][] Result,int N,int  P,int M){
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                //Funciona igual que el NaivStandard sin embargo se ahorra la declaración de una variable
+                //Funciona igual que el NaivStandard, sin embargo, se ahorra la declaración de una variable
                 Result[i][j] = 0;
                 for (int k = 0; k < P; k++) {
                     Result[i][j] += A[i][k]*B[k][j];
@@ -29,7 +29,7 @@ public class Algorithms {
         }
     }
 
-    //3-NaivKahan
+    // 3 NaivKahan
     /* Este método funciona igual que los dos anteriores sin embargo esta pensado para multiplicar matrices
        con valores doubles por eso tiene ñas varibales t,sum,err que lo que hacen es acumular el error que se puede generar
        al operar este tipo de numeros
@@ -51,7 +51,7 @@ public class Algorithms {
         }
     }
 
-    //4-NaivLoopUnrollingTwo
+    // 4 NaivLoopUnrollingTwo
     public static void NaivLoopUnrollingTwo(int[][]A,int[][] B,int[][] Result,int N,int P,int M) {
         int i, j, k;
         int aux;
@@ -67,11 +67,11 @@ public class Algorithms {
                 }
             }
         } else {
-            int PP = P - 1; //Si el numero de filas/columnas es impar se le quita uno a la P para obtener la posición del ultimo elemento
+            int PP = P - 1; //Si el número de filas/columnas es impar se le quita uno a la P para obtener la posición del ultimo elemento
             for (i = 0; i < N; i++) {
                 for (j = 0; j < M; j++) {
                     aux = 0;
-                    //Si es impar hace el mismo proceso, pero sin contar el utilimo elemento
+                    //Si es impar hace el mismo proceso, pero sin contar el último elemento
                     for (k = 0; k < PP; k += 2) {
                         aux += A[i][k]*B[k][j] + A[i][k+1]*B[k+1][j];
                     }
@@ -82,8 +82,8 @@ public class Algorithms {
         }
     }
 
-    //5-NaivLoopUnrollingThree
-    //Funciona muy parecido al anterior, solo que utilizando ternas
+    // 5 NaivLoopUnrollingThree
+    // Funciona muy parecido al anterior, solo que utilizando ternas
     public static void NaivLoopUnrollingThree(int[][]A,int[][] B,int[][] Result,int N,int P,int M) {
         int i, j, k;
         int aux;
@@ -100,8 +100,8 @@ public class Algorithms {
                 }
             }
         } else if (P % 3 == 1) {
-            //Si el numero de filas/columnas no es un multiplo de 3 y deja una posición sin evaluar
-            int PP = P - 1; //Se obtiene la ultima posición la cual no se va a evaluar dentro del for
+            //Si el número de filas/columnas no es un múltiplo de 3 y deja una posición sin evaluar
+            int PP = P - 1; //Se obtiene la última posición la cual no se va a evaluar dentro del for
             for (i = 0; i < N; i++) {
                 for (j = 0; j < M; j++) {
                     aux = 0;
@@ -114,8 +114,8 @@ public class Algorithms {
                 }
             }
         } else {
-            //Si el numero de filas/columnas no es un multiplo de 3 y deja dos posiciones sin evaluar
-            int PP = P - 2; //Se obtiene la penultima posición
+            //Si el número de filas/columnas no es un múltiplo de 3 y deja dos posiciones sin evaluar
+            int PP = P - 2; //Se obtiene la penúltima posición
             int PPP = P - 1;//Se obtiene la ultima posición la cual no se  va a evaluar dentro del for
             for (i = 0; i < N; i++) {
                 for (j = 0; j < M; j++) {
@@ -130,8 +130,9 @@ public class Algorithms {
             }
         }
     }
-    //6-NaivLoopUnrollingFour
-    //Funciona muy parecido que el anterior, solo que utilizando cuartas
+
+    // 6 NaivLoopUnrollingFour
+    // Funciona muy parecido que el anterior, solo que utilizando cuartas
     public static void NaivLoopUnrollingFour(int[][]A,int[][] B,int[][] Result,int N,int P,int M){
         int i, j, k;
         int aux;
@@ -164,9 +165,9 @@ public class Algorithms {
                 }
             }
         } else if (P % 4 == 2) {
-            //Si el numero de filas/columnas no es un multiplo de 4 y deja dos posiciones sin evaluar
-            int PP = P - 2; //Se obtiene la penultima posición la cual no se va a evaluar dentro del for
-            int PPP = P - 1; //Se obtiene la ultima posición la cual no se va a evaluar dentro del for
+            //Si el número de filas/columnas no es un múltiplo de 4 y deja dos posiciones sin evaluar
+            int PP = P - 2; //Se obtiene la penúltima posición la cual no se va a evaluar dentro del for
+            int PPP = P - 1; //Se obtiene la última posición la cual no se va a evaluar dentro del for
             for (i = 0; i < N; i++) {
                 for (j = 0; j < M; j++) {
                     aux = 0;
@@ -175,14 +176,14 @@ public class Algorithms {
                         aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j]
                                 + A[i][k + 3] * B[k + 3][j];
                     }
-                    //Se multiplica las ultimas dos posiciones que quedaron sin evaluar en el for anterior
+                    //Se multiplica las últimas dos posiciones que quedaron sin evaluar en el for anterior
                     Result[i][j] = aux + A[i][PP] * B[PP][j] + A[i][PPP] * B[PPP][j];
                 }
             }
         } else {
             int PP = P - 3; //Se obtiene la antepenúltima posición la cual no se va a evaluar dentro del for
-            int PPP = P - 2; //Se obtiene la penultima posición la cual no se va a evaluar dentro del for
-            int PPPP = P - 1; //Se obtiene la ultima posición la cual no se va a evaluar dentro del for
+            int PPP = P - 2; //Se obtiene la penúltima posición la cual no se va a evaluar dentro del for
+            int PPPP = P - 1; //Se obtiene la última posición la cual no se va a evaluar dentro del for
             for (i = 0; i < N; i++) {
                 for (j = 0; j < M; j++) {
                     aux = 0;
@@ -191,11 +192,139 @@ public class Algorithms {
                         aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j]
                                 + A[i][k + 3] * B[k + 3][j];
                     }
-                    //Se multiplica las ultimas tres posiciones que quedaron sin envaluar en el for anterior
+                    //Se multiplica las últimas tres posiciones que quedaron sin evaluar en el for anterior
                     Result[i][j] = aux + A[i][PP] * B[PP][j] + A[i][PPP] * B[PPP][j] + A[i][PPPP] * B[PPPP][j];
 
                 }
             }
         }
     }
+
+    // 7 - Strassen
+    public static int[][] Strassen(int[][] A, int[][] B) {
+        // Order of matrix
+        int n = A.length;
+
+        int[][] r = new int[n][n];
+
+        //SI SON MATRICES DE 1*1 AMBAS
+        if (n == 1) r[0][0] = A[0][0] * B[0][0];
+
+        else {
+            //crear 4 submatrices para ambas matrices del mismo tamaño
+            int[][] A11 = new int[n / 2][n / 2];
+            int[][] A12 = new int[n / 2][n / 2];
+            int[][] A21 = new int[n / 2][n / 2];
+            int[][] A22 = new int[n / 2][n / 2];
+            int[][] B11 = new int[n / 2][n / 2];
+            int[][] B12 = new int[n / 2][n / 2];
+            int[][] B21 = new int[n / 2][n / 2];
+            int[][] B22 = new int[n / 2][n / 2];
+
+            // LLena cada cuarto de matriz con la matriz correspondiente A o B
+            split(A, A11, 0, 0);
+            split(A, A12, 0, n / 2);
+            split(A, A21, n / 2, 0);
+            split(A, A22, n / 2, n / 2);
+            split(B, B11, 0, 0);
+            split(B, B12, 0, n / 2);
+            split(B, B21, n / 2, 0);
+            split(B, B22, n / 2, n / 2);
+
+            // Using Formulas as described in algorithm
+
+            // M1:=(A1+A3)×(B1+B2)
+            int[][] M1 = Strassen(add(A11, A22), add(B11, B22));
+
+            // M2:=(A2+A4)×(B3+B4)
+            int[][] M2 = Strassen(add(A21, A22), B11);
+
+            // M3:=(A1−A4)×(B1+A4)
+            int[][] M3 = Strassen(A11, sub(B12, B22));
+
+            // M4:=A1×(B2−B4)
+            int[][] M4 = Strassen(A22, sub(B21, B11));
+
+            // M5:=(A3+A4)×(B1)
+            int[][] M5 = Strassen(add(A11, A12), B22);
+
+            // M6:=(A1+A2)×(B4)
+            int[][] M6 = Strassen(sub(A21, A11), add(B11, B12));
+
+            // M7:=A4×(B3−B1)
+            int[][] M7 = Strassen(sub(A12, A22), add(B21, B22));
+
+            // P:=M2+M3−M6−M7
+            int[][] C11 = add(sub(add(M1, M4), M5), M7);
+
+            // Q:=M4+M6
+            int[][] C12 = add(M3, M5);
+
+            // R:=M5+M7
+            int[][] C21 = add(M2, M4);
+
+            // S:=M1−M3−M4−M5
+            int[][] C22 = add(sub(add(M1, M3), M2), M6);
+
+            // Step 3: Join 4 halves into one result matrix
+            join(C11, r, 0, 0);
+            join(C12, r, 0, n / 2);
+            join(C21, r, n / 2, 0);
+            join(C22, r, n / 2, n / 2);
+        }
+
+        return r;
+    }
+
+    // Method 2
+    // Function to subtract two matrices
+    private static int[][] sub(int[][] A, int[][] B){
+        int n = A.length;
+        int[][] C = new int[n][n];
+
+        //filas y columnas
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                C[i][j] = A[i][j] - B[i][j];
+
+        // Returning the resultant matrix
+        return C;
+    }
+
+    // Method 3
+    // Function to sum two matrices
+    private static int[][] add(int[][] A, int[][] B){
+        int n = A.length;
+        int[][] C = new int[n][n];
+
+        //filas y columnas
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                C[i][j] = A[i][j] + B[i][j];
+
+        // Returning the resultant matrix
+        return C;
+    }
+
+    // Method 4
+    // Function to split parent matrix
+    // into child matrices
+    private static void split(int[][] P, int[][] C, int iB, int jB){
+        //filas y columnas
+        for (int i1 = 0, i2 = iB; i1 < C.length; i1++, i2++)
+            for (int j1 = 0, j2 = jB; j1 < C.length; j1++, j2++)
+                C[i1][j1] = P[i2][j2];
+    }
+
+    // Method 5
+    // Function to join child matrices
+    // into (to) parent matrix
+    private static void join(int[][] C, int[][] P, int iB, int jB){
+        //filas y columnas
+        for (int i1 = 0, i2 = iB; i1 < C.length; i1++, i2++)
+            for (int j1 = 0, j2 = jB; j1 < C.length; j1++, j2++)
+                P[i2][j2] = C[i1][j1];
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,86 @@
+import javax.swing.*;
 import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[][] matriz = MatrixGenerator.loadMatrixFromFile(new File("matrix.txt"));
+        int[][] matrix1 = MatrixGenerator.loadMatrixFromFile(new File("matrix1.txt"));
+        int[][] matrix2 = MatrixGenerator.loadMatrixFromFile(new File("matrix2.txt"));
+        int[][] result = new int[matrix1.length][matrix1[0].length];
 
+        int option = 0;
+
+        while (true) {
+            option = Integer.parseInt(JOptionPane.showInputDialog(
+                    """
+                    Menú.
+                    1. NaivStandard\s
+                    2. NaivOnArray\s
+                    3. NaivKahan\s
+                    4. NaivLoopUnrollingTwo\s
+                    5. NaivLoopUnrollingThree\s
+                    6. NaivLoopUnrollingFour\s
+                    7. Strassen\s
+                    8. Salir\s
+                    """
+                    ));
+            if (option == 8) {
+                break;
+            }
+
+            long startTime, endTime;
+            switch (option) {
+                case 1:
+                    startTime = System.currentTimeMillis();
+                    Algorithms.NaivStandard(matrix1, matrix2, result, matrix1.length, matrix1[0].length, matrix2[0].length);
+                    endTime = System.currentTimeMillis();
+                    System.out.println("Tiempo de ejecución: " + (endTime - startTime) + " milisegundos");
+                    break;
+                case 2:
+                    startTime = System.currentTimeMillis();
+                    Algorithms.NaivOnArray(matrix1, matrix2, result, matrix1.length, matrix1[0].length, matrix2[0].length);
+                    endTime = System.currentTimeMillis();
+                    System.out.println("Tiempo de ejecución: " + (endTime - startTime) + " milisegundos");
+                    break;
+                case 3:
+                    double[][] resultDouble = new double[matrix1.length][matrix1[0].length];
+                    startTime = System.currentTimeMillis();
+                    Algorithms.NaivKahan(matrix1, matrix2, resultDouble, matrix1.length, matrix1[0].length, matrix2[0].length);
+                    endTime = System.currentTimeMillis();
+                    System.out.println("Tiempo de ejecución: " + (endTime - startTime) + " milisegundos");
+                    break;
+                case 4:
+                    startTime = System.currentTimeMillis();
+                    Algorithms.NaivLoopUnrollingTwo(matrix1, matrix2, result, matrix1.length, matrix1[0].length, matrix2[0].length);
+                    endTime = System.currentTimeMillis();
+                    System.out.println("Tiempo de ejecución: " + (endTime - startTime) + " milisegundos");
+                    break;
+                case 5:
+                    startTime = System.currentTimeMillis();
+                    Algorithms.NaivLoopUnrollingThree(matrix1, matrix2, result, matrix1.length, matrix1[0].length, matrix2[0].length);
+                    endTime = System.currentTimeMillis();
+                    System.out.println("Tiempo de ejecución: " + (endTime - startTime) + " milisegundos");
+                    break;
+                case 6:
+                    startTime = System.currentTimeMillis();
+                    Algorithms.NaivLoopUnrollingFour(matrix1, matrix2, result, matrix1.length, matrix1[0].length, matrix2[0].length);
+                    endTime = System.currentTimeMillis();
+                    System.out.println("Tiempo de ejecución: " + (endTime - startTime) + " milisegundos");
+                    break;
+                case 7:
+                    startTime = System.currentTimeMillis();
+                    result = Algorithms.Strassen(matrix1, matrix2);
+                    endTime = System.currentTimeMillis();
+                    System.out.println("Tiempo de ejecución: " + (endTime - startTime) + " milisegundos");
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+        }
+
+    }
+
+    public static void printMatrix(int[][] matriz) {
         for (int[] ints : matriz) {
             for (int j = 0; j < matriz[0].length; j++) {
                 System.out.print(ints[j] + " ");
