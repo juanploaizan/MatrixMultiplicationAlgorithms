fca1c3f7d061d4cfb6aafca7e3fe913d2cf53d6b
Alejandro Zambrano <josea.zambrano@uqvirtual.edu.co>
2023-10-25 23:49:32 -0500

    se documentaron brevemente los seis primeros algoritmos, queda faltando buscar el Strassen

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -59,6 +59,7 @@ public class Algorithms {
             for (i = 0; i < N; i++) {
                 for (j = 0; j < M; j++) {
                     aux = 0;
+                    //Si el numero de filas/columna es par, empieza a multiplicar por parejas (k y k+1) y amuenta en dos para continuar con la siguiente pareja
                     for (k = 0; k < P; k += 2) {
                         aux += A[i][k]*B[k][j] + A[i][k+1]*B[k+1][j];
                     }
@@ -66,13 +67,15 @@ public class Algorithms {
                 }
             }
         } else {
-            int PP = P - 1;
+            int PP = P - 1; //Si el numero de filas/columnas es impar se le quita uno a la P para obtener la posición del ultimo elemento
             for (i = 0; i < N; i++) {
                 for (j = 0; j < M; j++) {
                     aux = 0;
+                    //Si es impar hace el mismo proceso, pero sin contar el utilimo elemento
                     for (k = 0; k < PP; k += 2) {
                         aux += A[i][k]*B[k][j] + A[i][k+1]*B[k+1][j];
                     }
+                    //El ultimo elemento se multiplica aquí
                     Result[i][j] = aux + A[i][PP]*B[PP][j];
                 }
             }
@@ -80,13 +83,16 @@ public class Algorithms {
     }
 
     //5-NaivLoopUnrollingThree
+    //Funciona muy parecido al anterior, solo que utilizando ternas
     public static void NaivLoopUnrollingThree(int[][]A,int[][] B,int[][] Result,int N,int P,int M) {
         int i, j, k;
         int aux;
         if (P % 3 == 0) {
+            //Si el numero de filas/columnas es un numero multiplo de 3
             for (i = 0; i < N; i++) {
                 for (j = 0; j < M; j++) {
                     aux = 0;
+                    //Hará la multiplicación en forma de ternas y por eso aumentará el for en k+=3
                     for (k = 0; k < P; k += 3) {
                         aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j];
                     }
@@ -94,38 +100,47 @@ public class Algorithms {
                 }
             }
         } else if (P % 3 == 1) {
-            int PP = P - 1;
+            //Si el numero de filas/columnas no es un multiplo de 3 y deja una posición sin evaluar
+            int PP = P - 1; //Se obtiene la ultima posición la cual no se va a evaluar dentro del for
             for (i = 0; i < N; i++) {
                 for (j = 0; j < M; j++) {
                     aux = 0;
+                    //Hará la multiplicación en forma de ternas y por eso aumentará el for en k+=3
                     for (k = 0; k < PP; k += 3) {
                         aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j];
                     }
+                    //Se multiplica la ultima posición
                     Result[i][j] = aux + A[i][PP] * B[PP][j];
                 }
             }
         } else {
-            int PP = P - 2;
-            int PPP = P - 1;
+            //Si el numero de filas/columnas no es un multiplo de 3 y deja dos posiciones sin evaluar
+            int PP = P - 2; //Se obtiene la penultima posición
+            int PPP = P - 1;//Se obtiene la ultima posición la cual no se  va a evaluar dentro del for
             for (i = 0; i < N; i++) {
                 for (j = 0; j < M; j++) {
                     aux = 0;
+                    //Hará la multiplicación en forma de ternas y por eso aumentará el for en k+=3
                     for (k = 0; k < PP; k += 3) {
                         aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j];
                     }
+                    //Se multiplica las ultimas dos posiciones que quedaron sin evaluar en el for anterior
                     Result[i][j] = aux + A[i][PP] * B[PP][j] + A[i][PPP] * B[PPP][j];
                 }
             }
         }
     }
     //6-NaivLoopUnrollingFour
+    //Funciona muy parecido que el anterior, solo que utilizando cuartas
     public static void NaivLoopUnrollingFour(int[][]A,int[][] B,int[][] Result,int N,int P,int M){
         int i, j, k;
         int aux;
         if (P % 4 == 0) {
+            //Si el numero de filas/columnas es un numero multiplo de 4
             for (i = 0; i < N; i++) {
                 for (j = 0; j < M; j++) {
                     aux = 0;
+                    //Hará la multiplicación en forma de cuartas y por eso aumentará el for en k+=4
                     for (k = 0; k < P; k += 4) {
                         aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j]
                                 + A[i][k + 3] * B[k + 3][j];
@@ -134,44 +149,50 @@ public class Algorithms {
                 }
             }
         } else if (P % 4 == 1) {
-            int PP = P - 1;
+            //Si el numero de filas/columnas no es un multiplo de 4 y deja una posición sin evaluar
+            int PP = P - 1; //Se obtiene la ultima posición la cual no se va a evaluar dentro del for
             for (i = 0; i < N; i++) {
                 for (j = 0; j < M; j++) {
                     aux = 0;
+                    //Hará la multiplicación en forma de cuartas y por eso aumentará el for en k+=4
                     for (k = 0; k < PP; k += 4) {
                         aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j]
                                 + A[i][k + 3] * B[k + 3][j];
                     }
+                    //Se multiplica la ultima posicione que quedo sin evaluar en el for anterior
                     Result[i][j] = aux + A[i][PP] * B[PP][j];
                 }
             }
         } else if (P % 4 == 2) {
-            int PP = P - 2;
-            int PPP = P - 1;
+            //Si el numero de filas/columnas no es un multiplo de 4 y deja dos posiciones sin evaluar
+            int PP = P - 2; //Se obtiene la penultima posición la cual no se va a evaluar dentro del for
+            int PPP = P - 1; //Se obtiene la ultima posición la cual no se va a evaluar dentro del for
             for (i = 0; i < N; i++) {
                 for (j = 0; j < M; j++) {
                     aux = 0;
+                    //Hará la multiplicación en forma de cuartas y por eso aumentará el for en k+=4
                     for (k = 0; k < PP; k += 4) {
                         aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j]
                                 + A[i][k + 3] * B[k + 3][j];
                     }
+                    //Se multiplica las ultimas dos posiciones que quedaron sin evaluar en el for anterior
                     Result[i][j] = aux + A[i][PP] * B[PP][j] + A[i][PPP] * B[PPP][j];
                 }
             }
         } else {
-            int PP = P - 3;
-            int PPP = P - 2;
-            int PPPP = P - 1;
+            int PP = P - 3; //Se obtiene la antepenúltima posición la cual no se va a evaluar dentro del for
+            int PPP = P - 2; //Se obtiene la penultima posición la cual no se va a evaluar dentro del for
+            int PPPP = P - 1; //Se obtiene la ultima posición la cual no se va a evaluar dentro del for
             for (i = 0; i < N; i++) {
                 for (j = 0; j < M; j++) {
                     aux = 0;
+                    //Hará la multiplicación en forma de cuartas y por eso aumentará el for en k+=4
                     for (k = 0; k < PP; k += 4) {
                         aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j]
                                 + A[i][k + 3] * B[k + 3][j];
                     }
-                    Result[i][j] = aux + A[i][PP] * B[PP][j] + A[i][PPP] * B[PPP][j]
-
-                            + A[i][PPPP] * B[PPPP][j];
+                    //Se multiplica las ultimas tres posiciones que quedaron sin envaluar en el for anterior
+                    Result[i][j] = aux + A[i][PP] * B[PP][j] + A[i][PPP] * B[PPP][j] + A[i][PPPP] * B[PPPP][j];
 
                 }
             }
