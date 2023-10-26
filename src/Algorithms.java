646206edf6785046072b4d655c168a220688e5af
Alejandro Zambrano <josea.zambrano@uqvirtual.edu.co>
2023-10-25 23:18:39 -0500

    se agregaron los primeros seis algoritmos y se documentaron brevemente los tres primeros, falta buscar y agregar el algoritmo Strassen

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -6,6 +6,8 @@ public class Algorithms {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 aux = 0;
+                //Dado que la matriz es n*n recorre los elemento de la fila/columna
+                //de la matriz A/B y va sumando los resultados hasta llegar al final
                 for (int k = 0; k < P; k++) {
                     aux += A[i][k]*B[k][j];
                 }
@@ -18,10 +20,33 @@ public class Algorithms {
     public static void NaivOnArray(int[][]A,int[][] B,int[][] Result,int N,int  P,int M){
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
+                //Funciona igual que el NaivStandard sin embargo se ahorra la declaración de una variable
                 Result[i][j] = 0;
                 for (int k = 0; k < P; k++) {
                     Result[i][j] += A[i][k]*B[k][j];
                 }
+            }
+        }
+    }
+
+    //3-NaivKahan
+    /* Este método funciona igual que los dos anteriores sin embargo esta pensado para multiplicar matrices
+       con valores doubles por eso tiene ñas varibales t,sum,err que lo que hacen es acumular el error que se puede generar
+       al operar este tipo de numeros
+    */
+    public static void NaivKahan(int[][]A,int[][] B,double[][] Result,int N,int  P,int M){
+        double t, sum, err;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sum = 0.0;
+                err = 0.0;
+                for (int k = 0; k < P; k++) {
+                    err = err + A[i][k]*B[k][j];
+                    t = sum + err;
+                    err = (sum - t) + err;
+                    sum = t;
+                }
+                Result[i][j] = sum;
             }
         }
     }
@@ -54,8 +79,47 @@ public class Algorithms {
         }
     }
 
+    //5-NaivLoopUnrollingThree
+    public static void NaivLoopUnrollingThree(int[][]A,int[][] B,int[][] Result,int N,int P,int M) {
+        int i, j, k;
+        int aux;
+        if (P % 3 == 0) {
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < M; j++) {
+                    aux = 0;
+                    for (k = 0; k < P; k += 3) {
+                        aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j];
+                    }
+                    Result[i][j] = aux;
+                }
+            }
+        } else if (P % 3 == 1) {
+            int PP = P - 1;
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < M; j++) {
+                    aux = 0;
+                    for (k = 0; k < PP; k += 3) {
+                        aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j];
+                    }
+                    Result[i][j] = aux + A[i][PP] * B[PP][j];
+                }
+            }
+        } else {
+            int PP = P - 2;
+            int PPP = P - 1;
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < M; j++) {
+                    aux = 0;
+                    for (k = 0; k < PP; k += 3) {
+                        aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j];
+                    }
+                    Result[i][j] = aux + A[i][PP] * B[PP][j] + A[i][PPP] * B[PPP][j];
+                }
+            }
+        }
+    }
     //6-NaivLoopUnrollingFour
-    void NaivLoopUnrollingFour(int[][]A,int[][] B,int[][] Result,int N,int P,int M){
+    public static void NaivLoopUnrollingFour(int[][]A,int[][] B,int[][] Result,int N,int P,int M){
         int i, j, k;
         int aux;
         if (P % 4 == 0) {
